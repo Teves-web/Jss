e8743ceaba051202444d32b868fee9f0996e2b34
Teves-web <68256316+Teves-web@users.noreply.github.com>
2021-10-21 18:19:08 -0300

    Update palindromo.js

--- palindromo.js
+++ palindromo.js
@@ -5,3 +5,23 @@ function verificaPalindromo(string){
 }
 
 console.log(verificaPalindromo("ovo"));
+
+
+//solução 2
+
+//omo
+//012
+//abba
+//0123
+
+function verificaPalindromo(string){
+  if(string) return "string inexistente";
+  for(let i = 0; < string.length / 2; i++){
+    if(string[i] !== string [string.length -1 -i]){
+  
+     return false
+  }
+  }
+  return true
+}
+console.log(verificaPalindromo2("abba"));
