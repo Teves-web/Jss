c110a0c6f8cdd53cb4f8536965f1fbb74eceb4ec
Teves-web <68256316+Teves-web@users.noreply.github.com>
2021-10-21 22:20:43 -0300

    Update funcoes-autoinvocável-e-callbacks.js

--- funcoes-autoinvocável-e-callbacks.js
+++ funcoes-autoinvocável-e-callbacks.js
@@ -36,6 +36,24 @@ console.log(soma) //3
 //CALBACKS-> Uma função passada como argumento para outra função.
 //Utilizando callbacks, tem-se maior controle da ordem de chamadas.
 
+const calc = function(operacao, num1, num2){
+  return operacao(num1, num2);
+}
+
+const soma = function(num1, num2){
+  return num1 + num2;
+}
+
+const sub = function(num1, num2){
+  return num1 - num2;
+}
+
+const resultSoma = calc(soma, 1, 2);
+const resltSub = calc(sub, 1, 2);
+
+console.log(resltSub); //-1
+console.log(resultSoma); //3
+
 
 
 
