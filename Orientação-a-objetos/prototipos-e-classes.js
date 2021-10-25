ad19618b5ef55baa54a7cb0698973662059145f7
João Teves <68256316+Teves-web@users.noreply.github.com>
2021-10-25 02:39:14 -0300

    Create prototipos-e-classes.js

---
+++ Orientação-a-objetos/prototipos-e-classes.js
@@ -0,0 +1,4 @@
+//Protótipos -> Todos os objetos Javascript herdam propriedades e métodos de um prototype.
+// O objeto Object.prototype está no topo desta cadeia.
+
+// Classes-> Javascript não possui classes nativamente. todas as classes são objetos e a herança se dá por protótipos. (é uma sintaxe feita para facilitar a escrita
