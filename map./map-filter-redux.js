400749c2fa9bdac74a9b0dcf0fdd25e23c898244
João Teves <68256316+Teves-web@users.noreply.github.com>
2021-10-27 23:56:46 -0300

    Update map-filter-redux.js

--- map./map-filter-redux.js
+++ map./map-filter-redux.js
@@ -1,2 +1,3 @@
 //map-filter-reduce 
-sintaxe e map vs forEach->
+sintaxe e map vs forEach-> //Map-> Um conjunto passa a fazer parte de outro connjunto (ela cria um novo array);
+                                // Não moifica o array original; realiza as operações em ordem
